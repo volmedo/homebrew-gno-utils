d4328fb4391c0f76cd5f9c9d871b3077efcf0372
Vicente Olmedo <vicente.olmedo@gmail.com>
2023-05-02 19:13:24 +0200

    Add formula template for gnokey

---
+++ Formula/gnokey.rb
@@ -0,0 +1,30 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Gnokey < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/gnolang/gno/archive/refs/tags/chain/test3.2.tar.gz"
+  sha256 "0c9deeb4171d2c2c4fe281ae1d02caef8ba66c72868647e57aa2f70e2236a5ce"
+  license ""
+
+  depends_on "go" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test gnokey`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
