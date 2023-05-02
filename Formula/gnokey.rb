3abfcb29382e12494cb2de1f0ff954c070951dad
Vicente Olmedo <vicente.olmedo@gmail.com>
2023-05-02 19:16:03 +0200

    Add meaningful test for the formula

--- Formula/gnokey.rb
+++ Formula/gnokey.rb
@@ -17,15 +17,7 @@ class Gnokey < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test gnokey`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    # check gnokey works by using generate, which should output a passphrase of 24 random words
+    assert_match /^(\w+\s){23}\w+$/, shell_output("#{bin}/gnokey generate")
   end
 end
